2f656235597497e070f6a9c125712e1c8797de44
Kavindu-R <kavindu2804@outlook.com>
2024-09-10 13:13:21 +0530

    Footer Changes.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faTwitter, faInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"; // Added GitHub icon
+import { faFacebook, faTwitter, faInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"; 
 
 const Footer = () => {
   return (
