7b9ad8b983ad836977fd931923c105cfcd71c181
upekshaip <upekshaip@gmail.com>
2024-10-04 19:41:01 +0530

    completed auction page

--- src/pages/dashboardPages/Auctions.jsx
+++ src/pages/dashboardPages/Auctions.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import AuthNotification from "../../components/screens/AuthNotification";
+import { Link } from "react-router-dom";
 
 const AuctionPage = () => {
   const [auctions, setAuctions] = useState([]);
@@ -46,7 +47,6 @@ const AuctionPage = () => {
   };
 
   const filteredAuctions = auctions.filter((auction) => {
-    // Extract the date part from startTime and endTime
     const auctionStartDate = new Date(auction.startTime)
       .toISOString()
       .split("T")[0];
@@ -111,71 +111,127 @@ const AuctionPage = () => {
       </div>
 
       <div className="flex flex-wrap gap-4 mb-6">
-        <input
-          type="number"
-          name="startingBid"
-          placeholder="Starting Bid"
-          value={filters.startingBid}
-          onChange={handleFilterChange}
-          className="p-2 border border-gray-300 rounded-md"
-        />
-        <input
-          type="date"
-          name="startTime"
-          value={filters.startTime}
-          onChange={handleFilterChange}
-          className="p-2 border border-gray-300 rounded-md"
-        />
-        <input
-          type="date"
-          name="endTime"
-          value={filters.endTime}
-          onChange={handleFilterChange}
-          className="p-2 border border-gray-300 rounded-md"
-        />
-        <select
-          name="status"
-          value={filters.status}
-          onChange={handleFilterChange}
-          className="p-2 border border-gray-300 rounded-md"
-        >
-          <option value="">Select Status</option>
-          <option value="pending">Pending</option>
-          <option value="active">Active</option>
-          <option value="closed">Closed</option>
-        </select>
-        <select
-          name="category"
-          value={filters.category}
-          onChange={handleFilterChange}
-          className="p-2 border border-gray-300 rounded-md"
-        >
-          <option value="">Select Category</option>
-          <option value="Art and Antiques">Art and Antiques</option>
-          <option value="Fashion and Accessories">
-            Fashion and Accessories
-          </option>
-          <option value="Home and Garden">Home and Garden</option>
-          <option value="Electronics and Gadgets">
-            Electronics and Gadgets
-          </option>
-          <option value="Vehicles">Vehicles</option>
-          <option value="Collectibles">Collectibles</option>
-          <option value="Sports and Outdoors">Sports and Outdoors</option>
-          <option value="Toys and Games">Toys and Games</option>
-          <option value="Books and Media">Books and Media</option>
-          <option value="Photography">Photography</option>
-          <option value="Crafts and Hobbies">Crafts and Hobbies</option>
-          <option value="Real Estate">Real Estate</option>
-          <option value="Wine and Spirits">Wine and Spirits</option>
-          <option value="Tickets and Experiences">
-            Tickets and Experiences
-          </option>
-          <option value="Health and Beauty">Health and Beauty</option>
-        </select>
+        {/* Starting Bid Input */}
+        <div className="w-full md:w-auto">
+          <label
+            htmlFor="startingBid"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Starting Bid
+          </label>
+          <input
+            type="number"
+            name="startingBid"
+            id="startingBid"
+            placeholder="Starting Bid"
+            value={filters.startingBid}
+            onChange={handleFilterChange}
+            className="p-2 border border-gray-300 rounded-md"
+          />
+        </div>
+
+        {/* Start Time Input */}
+        <div className="w-full md:w-auto">
+          <label
+            htmlFor="startTime"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Start Time
+          </label>
+          <input
+            type="date"
+            name="startTime"
+            id="startTime"
+            value={filters.startTime}
+            onChange={handleFilterChange}
+            className="p-2 border border-gray-300 rounded-md"
+          />
+        </div>
+
+        {/* End Time Input */}
+        <div className="w-full md:w-auto">
+          <label
+            htmlFor="endTime"
+            className="block text-sm font-medium text-gray-700"
+          >
+            End Time
+          </label>
+          <input
+            type="date"
+            name="endTime"
+            id="endTime"
+            value={filters.endTime}
+            onChange={handleFilterChange}
+            className="p-2 border border-gray-300 rounded-md"
+          />
+        </div>
+
+        {/* Status Input */}
+        <div className="w-full md:w-auto">
+          <label
+            htmlFor="status"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Status
+          </label>
+          <select
+            name="status"
+            id="status"
+            value={filters.status}
+            onChange={handleFilterChange}
+            className="p-2 border border-gray-300 rounded-md"
+          >
+            <option value="">Select Status</option>
+            <option value="pending">Pending</option>
+            <option value="active">Active</option>
+            <option value="closed">Closed</option>
+          </select>
+        </div>
+
+        {/* Category Input */}
+        <div className="w-full md:w-auto">
+          <label
+            htmlFor="category"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Category
+          </label>
+          <select
+            name="category"
+            id="category"
+            value={filters.category}
+            onChange={handleFilterChange}
+            className="p-2 border border-gray-300 rounded-md"
+          >
+            <option value="">Select Category</option>
+            <option value="Art and Antiques">Art and Antiques</option>
+            <option value="Fashion and Accessories">
+              Fashion and Accessories
+            </option>
+            <option value="Home and Garden">Home and Garden</option>
+            <option value="Electronics and Gadgets">
+              Electronics and Gadgets
+            </option>
+            <option value="Vehicles">Vehicles</option>
+            <option value="Collectibles">Collectibles</option>
+            <option value="Sports and Outdoors">Sports and Outdoors</option>
+            <option value="Toys and Games">Toys and Games</option>
+            <option value="Books and Media">Books and Media</option>
+            <option value="Photography">Photography</option>
+            <option value="Crafts and Hobbies">Crafts and Hobbies</option>
+            <option value="Real Estate">Real Estate</option>
+            <option value="Wine and Spirits">Wine and Spirits</option>
+            <option value="Tickets and Experiences">
+              Tickets and Experiences
+            </option>
+            <option value="Health and Beauty">Health and Beauty</option>
+          </select>
+        </div>
+
+        {/* Reset Filters Button */}
         <button
           onClick={handleReset}
-          className="p-2 bg-gray-500 text-white rounded-md"
+          className="p-2 bg-gray-500 text-white rounded-md mt-6"
         >
           Reset Filters
         </button>
@@ -184,7 +240,8 @@ const AuctionPage = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {filteredAuctions.length > 0 ? (
           filteredAuctions.map((auction) => (
-            <div
+            <Link
+              to={`/auctions/${auction.auctionId}`}
               key={auction.auctionId}
               className="border rounded-lg shadow-md p-4"
             >
@@ -194,27 +251,29 @@ const AuctionPage = () => {
                 className="w-full h-48 object-cover rounded-md mb-4"
               />
               <h2 className="text-lg font-bold">{auction.title}</h2>
-              <p className="text-gray-700">{auction.description}</p>
-              <p className="text-gray-600">
+              <p className="text-gray-700 text-sm mb-2">
+                {auction.description}
+              </p>
+              <p className="text-gray-600 text-sm font-semibold">
                 Category: {auction.auctionCategory}
               </p>
-              <p className="text-gray-600">
+              <p className="text-gray-600 text-sm font-semibold">
                 Starting Bid: ${auction.startingBid}
               </p>
-              <p className="text-gray-600">
-                Start Date: {new Date(auction.startTime).toLocaleDateString()}
-              </p>
-              <p className="text-gray-600">
-                End Date: {new Date(auction.endTime).toLocaleDateString()}
-              </p>
               <p
-                className={`text-${
+                className={`text-sm my-2 text-${
                   auction.status === "active" ? "green" : "red"
-                }-600`}
+                }-600 font-semibold`}
               >
                 Status: {auction.status}
               </p>
-            </div>
+              <p className="text-gray-600 text-sm">
+                Start Date: {new Date(auction.startTime).toLocaleDateString()}
+              </p>
+              <p className="text-gray-600 text-sm">
+                End Date: {new Date(auction.endTime).toLocaleDateString()}
+              </p>
+            </Link>
           ))
         ) : (
           <p>No auctions found matching your criteria.</p>
