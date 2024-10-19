47fb7ad812bf213ba9a311998b748f8fcec00fe7
upekshaip <upekshaip@gmail.com>
2024-10-19 17:01:59 +0530

    need to update the Home.jsp page / new design needed @kavindu

--- src/pages/webPages/Home.jsx
+++ src/pages/webPages/Home.jsx
@@ -1,43 +1,8 @@
 import { motion } from "framer-motion";
-import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const HomePage = () => {
-  const carouselSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true,
-          autoplay: true,
-          autoplaySpeed: 3000,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 3000,
-        },
-      },
-    ],
-  };
-
   return (
     <div className="bg-[#000435] min-h-screen text-white overflow-hidden">
       <header className="text-center py-20">
@@ -86,78 +51,6 @@ const HomePage = () => {
           </motion.a>
         </motion.div>
       </header>
-
-      {/* Auction Items Carousel */}
-      <section className="py-16">
-        <motion.h2
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.8 }}
-          className="text-3xl font-semibold text-center"
-        >
-          Featured Auction Items
-        </motion.h2>
-
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.4, duration: 0.8 }}
-          className="mt-10"
-        >
-          <Slider {...carouselSettings}>
-            {/* Carousel Items */}
-            {[1, 2, 3, 4, 5].map((item) => (
-              <motion.div
-                key={item}
-                className="flex flex-col items-center text-center p-4"
-                whileHover={{ scale: 1.05 }}
-              >
-                <div className="bg-white p-4 rounded-lg shadow-lg text-black">
-                  <motion.img
-                    src={`https://via.placeholder.com/300?text=Item+${item}`}
-                    alt={`Item ${item}`}
-                    className="w-full h-48 object-cover rounded-lg"
-                    whileHover={{ scale: 1.1 }}
-                  />
-                  <motion.h3
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ delay: 0.5 }}
-                    className="text-xl font-semibold mt-4"
-                  >
-                    Auction Item {item}
-                  </motion.h3>
-                  <p className="text-gray-700 mt-2">Bid now starting at $100!</p>
-                </div>
-              </motion.div>
-            ))}
-          </Slider>
-        </motion.div>
-      </section>
-    </div>
-  );
-};
-
-const SampleNextArrow = (props) => {
-  const { onClick } = props;
-  return (
-    <div
-      className="slick-arrow slick-next text-black hover:text-yellow-400"
-      onClick={onClick}
-    >
-      <i className="fas fa-arrow-right"></i>
-    </div>
-  );
-};
-
-const SamplePrevArrow = (props) => {
-  const { onClick } = props;
-  return (
-    <div
-      className="slick-arrow slick-prev text-black hover:text-yellow-400"
-      onClick={onClick}
-    >
-      <i className="fas fa-arrow-left"></i>
     </div>
   );
 };
